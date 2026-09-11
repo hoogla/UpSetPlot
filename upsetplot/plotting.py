abbce729a93416f09861ff2bab1a7613fa9f61c8
hoogla <31314028+hoogla@users.noreply.github.com>
2026-09-11 09:59:43 +0200

    Update plotting.py
    Fixing https://github.com/jnothman/UpSetPlot/issues/301

--- upsetplot/plotting.py
+++ upsetplot/plotting.py
@@ -909,8 +909,10 @@ class UpSet:
             for rect in rects:
                 width = rect.get_width() + rect.get_x()
                 ax.text(
-                    width + margin,
-                    rect.get_y() + rect.get_height() * 0.5,
+                    #width + margin,
+                    #rect.get_y() + rect.get_height() * 0.5,
+                    float(np.ravel(width + margin)[0]),
+                    float(np.ravel(rect.get_y() + rect.get_height() * 0.5)[0]),
                     fmt.format(*make_args(width)),
                     ha="left",
                     va="center",
@@ -920,8 +922,10 @@ class UpSet:
             for rect in rects:
                 width = rect.get_width() + rect.get_x()
                 ax.text(
-                    width + margin,
-                    rect.get_y() + rect.get_height() * 0.5,
+                    #width + margin,
+                    #rect.get_y() + rect.get_height() * 0.5,
+                    float(np.ravel(width + margin)[0]),
+                    float(np.ravel(rect.get_y() + rect.get_height() * 0.5)[0]),
                     fmt.format(*make_args(width)),
                     ha="right",
                     va="center",
@@ -931,8 +935,10 @@ class UpSet:
             for rect in rects:
                 height = rect.get_height() + rect.get_y()
                 ax.text(
-                    rect.get_x() + rect.get_width() * 0.5,
-                    height + margin,
+                    #rect.get_x() + rect.get_width() * 0.5,
+                    #height + margin,
+                    float(np.ravel(rect.get_x() + rect.get_width() * 0.5)[0]),
+                    float(np.ravel(height + margin)[0]),
                     fmt.format(*make_args(height)),
                     ha="center",
                     va="bottom",
